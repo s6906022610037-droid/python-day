e6097d178ea16a06a237105daff9d77b5526989e
s6906022610037-droid <s6906022610037@email.kmutnb.ac.th>
2026-07-07 18:55:06 +0700

    fgfsg

---
+++ Lceture-02/test_input2.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ") 
+age = int(input("Enter your age: ")) 
+income = float(input("Enter your income: "))
+print ("The information you in put are:") 
+print ("Name:"+ name)
+print ("Age:"+ age) 
+print ("Income: ", format(income, ' 12,.2f '))
--- min.py
+++ min.py
@@ -1,1 +1,5 @@
-print("kwang")
+print("I'm hungry ")
+print('he ask me'"are you hungry")
+      
+print("I'm good and \"I will do my best\"")
+print('/\\')
