d00bc5b7d443e96413406d88db2b3a45c751b72c
kwang <https://github.com/s6906022610037-droid/kwang.html.git>
2026-09-24 00:30:54 +0700

    phet

--- Lceture-10/sqlitemodule.py
+++ Lceture-10/sqlitemodule.py
@@ -1,3 +1,23 @@
 import sqlite3 
 
-conn = sqlite3.connect('mydata')
+conn = sqlite3.connect('mydatabas.db') 
+cur = conn.cursor 
+
+cur.execute() 
+
+
+cur.execute()
+cur.execute() 
+cur.execute() 
+
+conn.commit() 
+
+cur.execute() 
+rows = cur.fetchall()  
+
+print("Users oldder then 28:")
+for row in rows: 
+    print(f"ID: {row[0]}, Name: {row[1]} , Age: {row[2]}, City: {row[3]}") 
+
+conn.close  
+
