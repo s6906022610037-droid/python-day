6e89f95a31b0373120729f46e80b6ebc37dfc1e9
kwang <https://github.com/s6906022610037-droid/kwang.html.git>
2026-09-24 09:10:55 +0700

    Perch

--- Lceture-10/requestsmmodule.py
+++ Lceture-10/requestsmmodule.py
@@ -8,7 +8,7 @@ if response.status_code == 200:
     
     print(f"Username : {user_data['login']}") 
     print(f"Name : {user_data['name']}") 
-    print(f"Bio{user_data['bio']}")
+    print(f"Bio : {user_data['bio']}")
     print(f"Public Repos : {user_data['public_repos']}") 
     print(f"Followers:{user_data['followers']}")
     print(f"Following:{user_data['following']}") 
